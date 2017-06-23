8287af9e08db7f9faed61fa0a2cab66a39f39747
byerman <694995626@qq.com>
2017-06-23 11:29:54 +0800

    新增矩形，圆角矩形，椭圆，圆形，圆弧的绘制

--- app/src/main/java/com/huaweisoft/customviewdemo/MainActivity.java
+++ app/src/main/java/com/huaweisoft/customviewdemo/MainActivity.java
@@ -2,11 +2,9 @@ package com.huaweisoft.customviewdemo;
 
 import android.content.DialogInterface;
 import android.graphics.Color;
-import android.graphics.ImageFormat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +14,7 @@ import com.huaweisoft.customviewdemo.View.MyView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private MyView myView;
-    private Button btnSetColor, btnClear, btnPoint;
+    private Button btnSetColor, btnClear, btnPoint,btnLine,btnRect,btnRoundRect,btnOval,btnCicle,btnArc;
     private int[] colors = new int[]{Color.BLUE, Color.GRAY, Color.GREEN, Color.RED, Color.YELLOW, Color.WHITE};
 
     @Override
@@ -32,12 +30,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnSetColor = (Button) findViewById(R.id.btn_setcolor);
         btnClear = (Button) findViewById(R.id.btn_clear);
         btnPoint = (Button) findViewById(R.id.btn_point);
+        btnLine = (Button) findViewById(R.id.btn_line);
+        btnRect = (Button) findViewById(R.id.btn_rect);
+        btnRoundRect = (Button) findViewById(R.id.btn_roundrect);
+        btnOval = (Button) findViewById(R.id.btn_oval);
+        btnCicle = (Button) findViewById(R.id.btn_cicle);
+        btnArc = (Button) findViewById(R.id.btn_arc);
     }
 
     private void initEvent() {
         btnSetColor.setOnClickListener(this);
         btnClear.setOnClickListener(this);
         btnPoint.setOnClickListener(this);
+        btnLine.setOnClickListener(this);
+        btnRect.setOnClickListener(this);
+        btnRoundRect.setOnClickListener(this);
+        btnOval.setOnClickListener(this);
+        btnCicle.setOnClickListener(this);
+        btnArc.setOnClickListener(this);
     }
 
     @Override
@@ -51,14 +61,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 myView.clear();
                 break;
             case R.id.btn_point:
-                selectPointNumber();
+                inputPointNumber();
+                break;
+            case R.id.btn_line:
+                inputLineNumber();
+                break;
+            case R.id.btn_rect:
+                inputRectNumber();
+                break;
+            case R.id.btn_roundrect:
+                inputRoundRectNumber();
+                break;
+            case R.id.btn_oval:
+                inputOvalNumber();
+                break;
+            case R.id.btn_cicle:
+                inputCicleNumber();
+                break;
+            case R.id.btn_arc:
+                inputArcNumber();
                 break;
             default:
                 break;
         }
     }
 
-    private void selectPointNumber(){
+    private void inputPointNumber(){
         final EditText inputEdit = new EditText(this);
         inputEdit.setFocusable(true);
 
@@ -71,13 +99,156 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void onClick(DialogInterface dialog, int which) {
                         String inputStr = inputEdit.getText().toString().replace("，",",");
                         String[] points = inputStr.split(",");
-                        Log.e("baiaj","points:"+points.toString());
                         float[] pointPts = new float[points.length];
                         for (int i = 0; i < points.length; i++) {
                             float pointNumber = Float.parseFloat(points[i]);
                             pointPts[i] = pointNumber;
                         }
                         myView.drawPoints(pointPts);
+                    }
+                });
+        mBuilder.show();
+    }
+
+    private void inputLineNumber(){
+        final EditText inputEdit = new EditText(this);
+        inputEdit.setFocusable(true);
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle(getString(R.string.input_line_pts))
+                .setView(inputEdit)
+                .setNegativeButton(getString(R.string.input_cancel),null)
+                .setPositiveButton(getString(R.string.input_concert), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputStr = inputEdit.getText().toString().replace("，",",");
+                        String[] lines = inputStr.split(",");
+                        float[] linePts = new float[lines.length];
+                        for (int i = 0; i < lines.length; i++) {
+                            float pointNumber = Float.parseFloat(lines[i]);
+                            linePts[i] = pointNumber;
+                        }
+                        myView.drawLines(linePts);
+                    }
+                });
+        mBuilder.show();
+    }
+
+    private void inputRectNumber(){
+        final EditText inputEdit = new EditText(this);
+        inputEdit.setFocusable(true);
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle(getString(R.string.input_rect_pts))
+                .setView(inputEdit)
+                .setNegativeButton(getString(R.string.input_cancel),null)
+                .setPositiveButton(getString(R.string.input_concert), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputStr = inputEdit.getText().toString().replace("，",",");
+                        String[] rects = inputStr.split(",");
+                        float[] rectPts = new float[rects.length];
+                        for (int i = 0; i < rects.length; i++) {
+                            float pointNumber = Float.parseFloat(rects[i]);
+                            rectPts[i] = pointNumber;
+                        }
+                        myView.drawRect(rectPts[0],rectPts[1],rectPts[2],rectPts[3]);
+                    }
+                });
+        mBuilder.show();
+    }
+
+    private void inputRoundRectNumber(){
+        final EditText inputEdit = new EditText(this);
+        inputEdit.setFocusable(true);
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle(getString(R.string.input_roundrect_pts))
+                .setView(inputEdit)
+                .setNegativeButton(getString(R.string.input_cancel),null)
+                .setPositiveButton(getString(R.string.input_concert), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputStr = inputEdit.getText().toString().replace("，",",");
+                        String[] rects = inputStr.split(",");
+                        float[] rectPts = new float[rects.length];
+                        for (int i = 0; i < rects.length; i++) {
+                            float pointNumber = Float.parseFloat(rects[i]);
+                            rectPts[i] = pointNumber;
+                        }
+                        myView.drawRoundRect(rectPts[0],rectPts[1],rectPts[2],rectPts[3],rectPts[4],rectPts[5]);
+                    }
+                });
+        mBuilder.show();
+    }
+
+    private void inputOvalNumber(){
+        final EditText inputEdit = new EditText(this);
+        inputEdit.setFocusable(true);
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle(getString(R.string.input_oval_pts))
+                .setView(inputEdit)
+                .setNegativeButton(getString(R.string.input_cancel),null)
+                .setPositiveButton(getString(R.string.input_concert), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputStr = inputEdit.getText().toString().replace("，",",");
+                        String[] rects = inputStr.split(",");
+                        float[] rectPts = new float[rects.length];
+                        for (int i = 0; i < rects.length; i++) {
+                            float pointNumber = Float.parseFloat(rects[i]);
+                            rectPts[i] = pointNumber;
+                        }
+                        myView.drawOval(rectPts[0],rectPts[1],rectPts[2],rectPts[3]);
+                    }
+                });
+        mBuilder.show();
+    }
+
+    private void inputCicleNumber(){
+        final EditText inputEdit = new EditText(this);
+        inputEdit.setFocusable(true);
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle(getString(R.string.input_cicle_pts))
+                .setView(inputEdit)
+                .setNegativeButton(getString(R.string.input_cancel),null)
+                .setPositiveButton(getString(R.string.input_concert), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputStr = inputEdit.getText().toString().replace("，",",");
+                        String[] rects = inputStr.split(",");
+                        float[] rectPts = new float[rects.length];
+                        for (int i = 0; i < rects.length; i++) {
+                            float pointNumber = Float.parseFloat(rects[i]);
+                            rectPts[i] = pointNumber;
+                        }
+                        myView.drawCicle(rectPts[0],rectPts[1],rectPts[2]);
+                    }
+                });
+        mBuilder.show();
+    }
+
+    private void inputArcNumber(){
+        final EditText inputEdit = new EditText(this);
+        inputEdit.setFocusable(true);
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle(getString(R.string.input_arc_pts))
+                .setView(inputEdit)
+                .setNegativeButton(getString(R.string.input_cancel),null)
+                .setPositiveButton(getString(R.string.input_concert), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputStr = inputEdit.getText().toString().replace("，",",");
+                        String[] rects = inputStr.split(",");
+                        float[] rectPts = new float[rects.length];
+                        for (int i = 0; i < rects.length; i++) {
+                            float pointNumber = Float.parseFloat(rects[i]);
+                            rectPts[i] = pointNumber;
+                        }
+                        myView.drawArc(rectPts[0],rectPts[1]);
                     }
                 });
         mBuilder.show();
--- app/src/main/java/com/huaweisoft/customviewdemo/View/MyView.java
+++ app/src/main/java/com/huaweisoft/customviewdemo/View/MyView.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -16,10 +18,27 @@ import android.view.View;
 
 public class MyView extends View {
 
-//    private Canvas mCanvas;
+    //    private Canvas mCanvas;
     private Paint mPaint = new Paint();
+    // 底色
     private int mColor = -1;
+    // 点坐标
     private float[] ponitPts = new float[]{};
+    // 线坐标
+    private float[] linePts = new float[]{};
+    // 矩形坐标
+    private float rectX1, rectX2, rectY1, rectY2;
+    // 圆角矩形坐标
+    private float roundX1, roundY1, roundX2, roundY2, roundRx, roundRy;
+    // 椭圆坐标
+    private float ovalX1, ovalY1, ovalX2, ovalY2;
+    // 圆坐标
+    private float cicleX1, cicleY1, radiu;
+    // 圆弧角度
+    private float arcStartAngle = -1;
+    private float arcSweepAngle = -1;
+    // paint的颜色
+    private int paintColor = Color.BLACK;
 
     public MyView(Context context) {
         super(context);
@@ -47,11 +66,35 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Log.e("BAIAJ", "onDraw");
-        if (mColor != -1){
+        if (mColor != -1) {
             canvas.drawColor(mColor);
         }
-        if (ponitPts != null){
-            canvas.drawPoints(ponitPts,mPaint);
+        if (ponitPts != null && ponitPts.length >= 2) {
+            canvas.drawPoints(ponitPts, mPaint);
+        }
+        if (linePts != null && linePts.length >= 2) {
+            canvas.drawLines(linePts, mPaint);
+        }
+        if (rectX1 != 0) {
+            RectF rect = new RectF(rectX1, rectY1, rectX2, rectY2);
+            canvas.drawRect(rect, mPaint);
+        }
+        if (roundX1 != 0) {
+            RectF rect = new RectF(roundX1, roundY1, roundX2, roundY2);
+            canvas.drawRoundRect(rect, roundRx, roundRy, mPaint);
+        }
+        if (ovalX1 != 0) {
+            RectF rectF = new RectF(ovalX1, ovalY1, ovalX2, ovalY2);
+            canvas.drawOval(rectF, mPaint);
+        }
+        if (cicleX1 != 0) {
+            canvas.drawCircle(cicleX1, cicleY1, radiu, mPaint);
+        }
+        if (arcStartAngle != -1 && rectX1 != 0){
+            RectF rect = new RectF(rectX1, rectY1, rectX2, rectY2);
+            mPaint.setColor(Color.BLUE);
+            canvas.drawArc(rect,arcStartAngle,arcSweepAngle,true,mPaint);
+            mPaint.setColor(Color.BLACK);
         }
     }
 
@@ -67,15 +110,57 @@ public class MyView extends View {
     }
 
     public void clear() {
+        mColor = -1;
+        ponitPts = null;
+        linePts = null;
+        rectX1 = 0;
+        roundX1 = 0;
+        ovalX1 = 0;
+        cicleX1 = 0;
+        arcStartAngle = -1;
         invalidate();
     }
 
-    public void drawPoints(float[] pts){
+    public void drawPoints(float[] pts) {
         ponitPts = pts;
     }
 
-    public void drawLine() {
+    public void drawLines(float[] pts) {
+        linePts = pts;
+    }
 
+    public void drawRect(float x1, float y1, float x2, float y2) {
+        rectX1 = x1;
+        rectX2 = x2;
+        rectY1 = y1;
+        rectY2 = y2;
+    }
+
+    public void drawRoundRect(float x1, float y1, float x2, float y2, float rx, float ry) {
+        roundX1 = x1;
+        roundX2 = x2;
+        roundY1 = y1;
+        roundY2 = y2;
+        roundRx = rx;
+        roundRy = ry;
+    }
+
+    public void drawOval(float x1, float y1, float x2, float y2) {
+        ovalX1 = x1;
+        ovalY1 = y1;
+        ovalX2 = x2;
+        ovalY2 = y2;
+    }
+
+    public void drawCicle(float x1, float y1, float radius) {
+        cicleX1 = x1;
+        cicleY1 = y1;
+        radiu = radius;
+    }
+
+    public void drawArc(float startAngle, float sweepAngle){
+        arcStartAngle = startAngle;
+        arcSweepAngle = sweepAngle;
     }
 
 }
